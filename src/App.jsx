9bec91e1a8f18c13c4f3113f947a76828c3a15cc
ozi <ozisadiqkasai@gmail.com>
2023-09-21 16:34:15 +0100

    initital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+     hello react
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase.jsx
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIi8s7Rfi7Zk7EMGhrAloCmPsk6mVmGqs',
+  authDomain: 'clone-894df.firebaseapp.com',
+  projectId: 'clone-894df',
+  storageBucket: 'clone-894df.appspot.com',
+  messagingSenderId: '812951778089',
+  appId: '1:812951778089:web:5248d75e7dd82470b2fd38',
+  // measurementId: 'G-W6RL1FR8DK',
+}

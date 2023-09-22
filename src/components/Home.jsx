d2ea31c15133d3e1935901c687b710978aeba2f0
ozi <ozisadiqkasai@gmail.com>
2023-09-22 16:24:34 +0100

    added prime to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,10 @@
+import './Home.css'
 const Home = () => {
   return (
     <div>
-     <div className="home__container">
-      <img src="https://imagess-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Gemeroc/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220" alt="" />
-     </div>
+      <div className="home__container">
+        <img className="home__Image" src="src/assets/banner 1.jpg" alt="" />
+      </div>
     </div>
   )
 }
